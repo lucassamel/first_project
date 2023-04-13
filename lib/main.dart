0a1ac1ce97445b3ca41d740835ef3f5108606d16
Lucas Samel <Lucas.Samel@fadami.com.br>
2023-04-13 18:59:06 -0300

    Tratamento de erro para o carregamento da imagem.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:first_project/screens/form_screen.dart';
 import 'package:flutter/material.dart';
 
-import 'screens/initial_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const InicialScreen(),
+      home: const FormScreen(),
     );
   }
 }
